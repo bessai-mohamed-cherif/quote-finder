3ea8046838f797940ad2f461466264be06225810
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-04-08 19:12:43 +0100

    adding topics 4

--- topics/views.py
+++ topics/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from search.models import Quotes
 
 
 # Create your views here.
 def alltopics(request):
     topics = ["life", "love", "war"]
-    return render(request,"topics/alltopics.html",{"topics":topics})
+    return render(request, "topics/alltopics.html", {"topics": topics})
 
 def detail(request, topic):
-    return render(request, "topics/detail.html", {"topic": topic})
+    results = Quotes.objects.filter(topics__contains=topic)
+    return render(request, "topics/detail.html", {"results": results})
