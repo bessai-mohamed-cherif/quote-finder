e614e038674ea7446e880ad344d8786fce870c4a
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-04-10 14:17:29 +0100

    rearranging authors 2

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,9 @@ from .models import Authors
 # Create your views here.
 def allauthors(request):
     authors = Authors.objects.all().order_by('-name')
+    authorstable = {}
     for author in authors:
-        authorstable = {author.name:Authors.objects.filter(name=author.name).count()}
+        authorstable[author.name] = Authors.objects.filter(name=author.name).count()
+
 
     return render(request,"authors/allauthors.html",{"authorstable":authorstable})
