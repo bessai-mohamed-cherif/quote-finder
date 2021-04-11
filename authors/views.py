b5ecabfe14360e2a7fc7e6afd01255601da34f69
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-04-11 09:10:14 +0100

    adding authors

--- authors/views.py
+++ authors/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from .models import Authors
+from search.models import Quotes
 
 # Create your views here.
 def allauthors(request):
     authors = Authors.objects.all().order_by('-name')
     authorstable = {}
     for author in authors:
-        authorstable[author.name] = Authors.objects.filter(name=author.name).count()
+        authorstable[author.name] = Quotes.objects.filter(author=author.name).count()
     return render(request,"authors/allauthors.html",{"authorstable":authorstable})
 
 def detailauthor(request, key):
     authors = Authors.objects.filter(name=key)
-    print(authors)
     return render(request, "authors/detailauthor.html", {'authors': authors})
